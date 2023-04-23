55f60d4d4e2ed3306b0d6a754a09bf785c099c7b
vixxlol <iguliszek@proton.me>
2023-04-23 11:37:50 +0200

    Added color variables

---
+++ main.sh
@@ -0,0 +1,9 @@
+# Define color variables
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+PURPLE=$(tput setaf 25)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+RESET=$(tput sgr0)
