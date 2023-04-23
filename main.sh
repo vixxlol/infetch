609fa0e91fd1a9e492927fd7190aa015a64a5c70
vixxlol <iguliszek@proton.me>
2023-04-23 11:43:47 +0200

    Added comments

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 # Define color variables
+
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
@@ -8,9 +9,17 @@ CYAN=$(tput setaf 6)
 WHITE=$(tput setaf 7)
 RESET=$(tput sgr0)
 
+# Echo everything
+#
+# Echo username and hostname like this: username@hostname
 echo "${RED}${USER}${RESET}@${YELLOW}${HOSTNAME}${RESET}"
+# Echo username
 echo "${RED}Username: ${RESET}$(whoami)"
+# Echo hostname
 echo "${YELLOW}Hostname: ${RESET}$(hostname)"
+# Echo distro
 echo "${PURPLE}Distro: ${RESET}$(cat /etc/*release | grep PRETTY_NAME | cut -d '"' -f 2)"
+# Echo shell
 echo "${CYAN}Shell: ${RESET}$(which $SHELL)"
+# Echo uptime
 echo "${GREEN}Uptime: ${RESET}$(uptime | awk '{print $1}' | cut -d "," -f 1)"
